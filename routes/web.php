de38e224743298edef9f2a9fb1bb3da888c0c510
lothithanhhuonga6-web <lothithanhhuonga6@gmail.com>
2026-03-17 10:04:16 +0700

    lo thi thanh huong

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ Route::get('/sach-kinh-dien', [BookController::class, 'laySachKinhDien']);
 Route::get('/nguyennghia-gif', function () {
     return 'nguyennghia-gif';
 });
+
+Route::get('/lothithanhhuonga6-web', function () {
+    return 'lothithanhhuonga6-web';
+});

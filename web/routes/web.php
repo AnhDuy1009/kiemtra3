0da6fa9c06475fa356e149d6adbff42768319be1
nguyennghia <nghiatracau@gmail.com>
2026-03-17 09:46:44 +0700

    nguyennghia

--- web/routes/web.php
+++ web/routes/web.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
 Route::get('/sach-kinh-dien', [BookController::class, 'laySachKinhDien']);      
+Route::get('/nguyennghia-gif', function () {
+    return 'nguyennghia-gif';
+});

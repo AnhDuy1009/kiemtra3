41aad32abfbfe6f48d051540763296d4ed190ed7
phandinhphuc2108-gif <phandinhphuc2108@gmail.com>
2026-03-17 09:55:59 +0700

    Add route for Phan Dinh Phuc

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
 Route::get('/sach-kinh-dien', [BookController::class, 'laySachKinhDien']);
+Route::get('/phandinhphuc2108-gif', function () {
+    return 'Phan Dinh Phuc';
+});
